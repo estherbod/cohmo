da5aede5f5e7d2a4dab9f44ddb9e496acdc51881
Giada Franz <walypala23@gmail.com>
2018-03-25 23:58:18 +0200

    Instance config

---
+++ instance/config.py
@@ -0,0 +1,5 @@
+DEBUG = True
+TEAMS_FILE_PATH = 'test_data/teams.txt'
+TABLE_FILE_PATHS = {'T1': 'test_data/T1.txt',
+                    'T8': 'test_data/T8.txt'}
+HISTORY_FILE_PATH = 'test_data/history.txt'
