b41d393d8a74c3b38add0cad999d54165f1bd800
Federico Glaudo <dario2994@gmail.com>
2018-03-31 23:47:09 +0200

    Sketch of queues page

---
+++ cohmo/static/queues.js
@@ -0,0 +1,45 @@
+Vue.options.delimiters = ['[[', ']]'];
+
+const TableStatus = Object.freeze({CALLING: 0, CORRECTING: 1, IDLE: 2});
+const TableStatusName = ['Calling', 'Coordination', 'Idle'];
+
+let queues_model = {
+    tables: {},
+    last_update: -1,
+    update() {
+        axios.get('/tables/get_all', {params: {last_update: this.last_update}})
+            .then(response => {
+                if (!response.data.ok) {
+                    console.log('TODO');
+                    return;
+                }
+                if (!response.data.changed) return;
+                this.last_update = response.data.last_update;
+                Object.assign(this.tables, JSON.parse(response.data.tables));
+                queues_component.$forceUpdate();
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+};
+queues_model.update();
+
+
+const queues_component = new Vue({
+    el: '#queues',
+    data: {
+        tables: queues_model.tables,
+        TableStatus: TableStatus,
+        TableStatusName: TableStatusName,
+    },
+    methods: {
+        human_readable_time: function(timestamp) {
+            var date = new Date(timestamp*1000);
+            var hours = date.getHours();
+            var minutes = "0" + date.getMinutes();
+            var seconds = "0" + date.getSeconds();
+            return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+        }
+    }
+});
