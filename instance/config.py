bc8c741854be9d585f3cf09a76d963a728d2a53f
Giada Franz <walypala23@gmail.com>
2018-04-02 22:22:44 +0200

    Increased test_data

--- instance/config.py
+++ instance/config.py
@@ -1,5 +1,15 @@
 DEBUG = True
 TEAMS_FILE_PATH = 'test_data/teams.txt'
 TABLE_FILE_PATHS = {'T1': 'test_data/T1.txt',
-                    'T8': 'test_data/T8.txt'}
+                    'T2': 'test_data/T2.txt',
+                    'T3': 'test_data/T3.txt',
+                    'T4': 'test_data/T4.txt',
+                    'T5': 'test_data/T5.txt',
+                    'T6': 'test_data/T6.txt',
+                    'T7': 'test_data/T7.txt',
+                    'T8': 'test_data/T8.txt',
+                    'T9': 'test_data/T9.txt',
+                    'T10': 'test_data/T10.txt',
+                    'T11': 'test_data/T11.txt',
+                    'T12': 'test_data/T12.txt'}
 HISTORY_FILE_PATH = 'test_data/history.txt'
