7c3808e47b36b4d8c65deb71ed77dac20e88eb40
Esther Bod <esther.bod@gmail.com>
2019-08-10 18:27:49 +0200

    Add some extra tables.

--- instance/config.py
+++ instance/config.py
@@ -1,16 +1,22 @@
 DEBUG = True
 TEAMS_FILE_PATH = 'test_data/teams.txt'
-TABLE_FILE_PATHS = {'T1': 'test_data/T1.json',
-                    'T2': 'test_data/T2.json',
-                    'T3': 'test_data/T3.json',
-                    'T4': 'test_data/T4.json',
-                    'T5': 'test_data/T5.json',
-                    'T6': 'test_data/T6.json',
-                    'T7': 'test_data/T7.json',
-                    'T8': 'test_data/T8.json',
-                    'T9': 'test_data/T9.json',
-                    'T10': 'test_data/T10.json',
-                    'T11': 'test_data/T11.json',
-                    'T12': 'test_data/T12.json'}
+TABLE_FILE_PATHS = {'1A': 'test_data/T1A.json',
+                    '1B': 'test_data/T1B.json',
+                    '1C': 'test_data/T1C.json',
+                    '2A': 'test_data/T2A.json',
+                    '2B': 'test_data/T2B.json',
+                    '2C': 'test_data/T2C.json',
+                    '3A': 'test_data/T3A.json',
+                    '3B': 'test_data/T3B.json',
+                    '4A': 'test_data/T4A.json',
+                    '4B': 'test_data/T4B.json',
+                    '4C': 'test_data/T4C.json',
+                    '4D': 'test_data/T4D.json',
+                    '5A': 'test_data/T5A.json',
+                    '5B': 'test_data/T5B.json',
+                    '5C': 'test_data/T5C.json',
+                    '5D': 'test_data/T5D.json',
+                    '6A': 'test_data/T6A.json',
+                    '6B': 'test_data/T6B.json'}
 HISTORY_FILE_PATH = 'test_data/history.csv'
 AUTHENTICATION_FILE_PATH = 'test_data/auth_list.json'
